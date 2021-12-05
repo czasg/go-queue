36a9e7513645834bdc5c14642ef3266552634eda
czasg <czasg0.0>
2021-12-05 22:26:33 +0800

    feat: 增加 lifo_disk_queue

---
+++ lifo_disk_queue.go
@@ -0,0 +1,190 @@
+package queue
+
+import (
+	"bytes"
+	"encoding/binary"
+	"encoding/json"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"sync"
+)
+
+const (
+	LIFO_STAT = "lifo.stat.json"
+	LIFO_DATA = "lifo.data"
+)
+
+func NewLifoDiskQueue(dir string) (Queue, error) {
+	dir = filepath.Join(dir, "_lifo")
+	queue := LifoDiskQueue{Dir: dir}
+	err := os.MkdirAll(dir, os.ModePerm)
+	if err != nil {
+		return nil, err
+	}
+	stat, err := queue.openStat()
+	if err != nil {
+		return nil, err
+	}
+	if stat == nil {
+		stat = &LifoStat{}
+	}
+	queue.Stat = stat
+	head, err := queue.openHead()
+	if err != nil {
+		return nil, err
+	}
+	_, err = head.Seek(0, os.SEEK_END)
+	if err != nil {
+		return nil, err
+	}
+	queue.HeadFile = head
+	return &queue, nil
+}
+
+var _ Queue = (*LifoDiskQueue)(nil)
+
+type LifoDiskQueue struct {
+	Dir      string
+	Stat     *LifoStat
+	HeadFile *os.File
+	TailFile *os.File
+	Lock     sync.Mutex
+	Closed   bool
+}
+
+type LifoStat struct {
+	Size   int
+	Offset int
+}
+
+func (q *LifoDiskQueue) Push(data []byte) error {
+	if q.Closed {
+		return ErrClosed
+	}
+	q.Lock.Lock()
+	defer q.Lock.Unlock()
+	buf := new(bytes.Buffer)
+	err := binary.Write(buf, binary.BigEndian, int32(len(data)))
+	if err != nil {
+		return err
+	}
+	_, err = q.HeadFile.Write(bytes.Join([][]byte{data, buf.Bytes()}, []byte("")))
+	if err != nil {
+		return err
+	}
+	q.Stat.Size++
+	q.Stat.Offset += 4 + len(data)
+	return nil
+}
+
+func (q *LifoDiskQueue) Pop() ([]byte, error) {
+	if q.Closed {
+		return nil, ErrClosed
+	}
+	if q.Stat.Size < 1 {
+		return nil, ErrEmptyQueue
+	}
+	q.Lock.Lock()
+	defer q.Lock.Unlock()
+	_, err := q.HeadFile.Seek(-4, os.SEEK_CUR)
+	if err != nil {
+		return nil, err
+	}
+	buf := make([]byte, 4)
+	_, err = q.HeadFile.Read(buf)
+	if err != nil {
+		return nil, err
+	}
+	var length int32
+	err = binary.Read(bytes.NewBuffer(buf), binary.BigEndian, &length)
+	if err != nil {
+		return nil, err
+	}
+	_, err = q.HeadFile.Seek(int64(-4-length), os.SEEK_CUR)
+	if err != nil {
+		return nil, err
+	}
+	buf = make([]byte, length)
+	_, err = q.HeadFile.Read(buf)
+	if err != nil {
+		return nil, err
+	}
+	_, err = q.HeadFile.Seek(int64(-length), os.SEEK_CUR)
+	if err != nil {
+		return nil, err
+	}
+	err = q.HeadFile.Truncate(int64(q.Stat.Offset) - int64(4+length))
+	if err != nil {
+		return nil, err
+	}
+	q.Stat.Size--
+	q.Stat.Offset -= 4 + int(length)
+	return buf, nil
+}
+
+func (q *LifoDiskQueue) Close() error {
+	q.Lock.Lock()
+	defer q.Lock.Unlock()
+	if q.Closed {
+		return nil
+	}
+	q.Closed = true
+	_ = q.HeadFile.Close()
+	err := q.saveStat()
+	if err != nil {
+		return err
+	}
+	if q.Stat.Size > 0 {
+		return nil
+	}
+	return os.RemoveAll(q.Dir)
+}
+
+func (q *LifoDiskQueue) Len() int {
+	return q.Stat.Size
+}
+
+func (q *LifoDiskQueue) openStat() (*LifoStat, error) {
+	statJsonPath := filepath.Join(q.Dir, LIFO_STAT)
+	_, err := os.Stat(statJsonPath)
+	if os.IsNotExist(err) {
+		return nil, nil
+	}
+	f, err := os.Open(statJsonPath)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	body, err := ioutil.ReadAll(f)
+	if err != nil {
+		return nil, err
+	}
+	var stat LifoStat
+	err = json.Unmarshal(body, &stat)
+	if err != nil {
+		return nil, err
+	}
+	return &stat, nil
+}
+
+func (q *LifoDiskQueue) saveStat() error {
+	statJsonPath := filepath.Join(q.Dir, LIFO_STAT)
+	f, err := os.Create(statJsonPath)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	body, _ := json.MarshalIndent(q.Stat, "", "  ")
+	_, err = f.Write(body)
+	return err
+}
+
+func (q *LifoDiskQueue) openHead() (*os.File, error) {
+	dataPath := filepath.Join(q.Dir, LIFO_DATA)
+	f, err := os.OpenFile(dataPath, os.O_RDWR|os.O_CREATE, os.ModePerm)
+	if err != nil {
+		return nil, err
+	}
+	return f, nil
+}
---
+++ lifo_disk_queue_test.go
@@ -0,0 +1,51 @@
+package queue
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+)
+
+func TestNewLifoDiskQueue(t *testing.T) {
+	dir, err := ioutil.TempDir("", "")
+	if err != nil {
+		panic(err)
+	}
+	defer os.RemoveAll(dir)
+	queue, err := NewLifoDiskQueue(dir)
+	if err != nil {
+		panic(err)
+	}
+	queue.Len()
+	assertErr(t, queue.Push([]byte{1}), nil)
+	assertErr(t, queue.Push([]byte{2}), nil)
+	assertErr(t, queue.Push([]byte{3}), nil)
+	assertErr(t, queue.Push([]byte{4}), nil)
+	assertErr(t, queue.Push([]byte{5}), nil)
+	err = queue.Close()
+	if err != nil {
+		panic(err)
+	}
+	queue, err = NewLifoDiskQueue(dir)
+	if err != nil {
+		panic(err)
+	}
+	data, err := queue.Pop()
+	assertErr(t, err, nil)
+	assertData(t, data, []byte{5})
+	data, err = queue.Pop()
+	assertErr(t, err, nil)
+	assertData(t, data, []byte{4})
+	data, err = queue.Pop()
+	assertErr(t, err, nil)
+	assertData(t, data, []byte{3})
+	data, err = queue.Pop()
+	assertErr(t, err, nil)
+	assertData(t, data, []byte{2})
+	data, err = queue.Pop()
+	assertErr(t, err, nil)
+	assertData(t, data, []byte{1})
+	data, err = queue.Pop()
+	assertErr(t, err, ErrEmptyQueue)
+	assertData(t, data, nil)
+}
